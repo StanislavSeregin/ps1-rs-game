70c5f808b00b4fe062a9efd32f43864f49d2ff15
StanislavSeregin <stas_seregin@mail.ru>
2025-02-01 22:22:13 +0300

    Add hello world

---
+++ README.md
@@ -0,0 +1,6 @@
+# PS1 Demo
+
+## Build
+```sh
+cargo psx build --toolchain nightly-2024-12-21
+```
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+use psx::gpu::VideoMode;
+use psx::{dprintln, Framebuffer};
+
+#[unsafe(no_mangle)]
+fn main() {
+    let buf0 = (0, 0);
+    let buf1 = (0, 240);
+    let res = (320, 240);
+    let txt_offset = (0, 8);
+    let mut fb = Framebuffer::new(buf0, buf1, res, VideoMode::NTSC, None).unwrap();
+    let font = fb.load_default_font();
+    let mut txt = font.new_text_box(txt_offset, res);
+    loop {
+        txt.reset();
+        dprintln!(txt, "Hello, world!");
+        fb.draw_sync();
+        fb.wait_vblank();
+        fb.swap();
+    }
+}
