8aad2eac96d81dbf9b7dbd316d0518d556736798
StanislavSeregin <stas_seregin@mail.ru>
2025-02-12 21:17:18 +0300

    Try to add debug

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,49 @@
 mod common;
 mod spu;
 
+use core::borrow;
+use core::cell::RefCell;
+
+use psx::gpu::VideoMode;
+use psx::{dprintln, Framebuffer, TextBox};
 use psx::include_words;
-use spu::spu::SpuUpload;
+use spu::SpuUpload;
 
 #[unsafe(no_mangle)]
 fn main() {
+    let mut debug_func = init_debug();
+
+    debug_func("srak!!!");
+
     let audio_sample = include_words!("./../assets/audio/test.adpcm");
     let mut spu_upload = SpuUpload::new();
-    let mut spu_sample = spu_upload.load(audio_sample);
-    spu_sample.play();
+    let mut spu_sample = spu_upload.load(audio_sample, &debug_func);
+    spu_sample.play(&debug_func);
 
     loop {
         // TODO
+    }
+}
+
+fn init_debug() -> impl Fn(&str) {
+    let buf0 = (0, 0);
+    let buf1 = (0, 240);
+    let res = (320, 240);
+    let txt_offset = (0, 64);
+    let mut fb = Framebuffer::new(buf0, buf1, res, VideoMode::NTSC, None).unwrap();
+    let font = fb.load_default_font();
+    let txt = RefCell::new((font.new_text_box(txt_offset, res), fb));
+
+    move |message: &str| {
+        let mut txt = txt.borrow_mut();
+        txt.0.reset();
+        for ch in message.as_bytes() {
+            txt.0.print_char(ch.clone());
+            txt.0.move_right(1);
+        }
+        
+        txt.1.draw_sync();
+        txt.1.wait_vblank();
+        txt.1.swap();
     }
 }
--- src/spu.rs
+++ src/spu.rs
@@ -1,64 +1,74 @@
-pub mod spu {
-    use crate::common::MemoryCell;
+use core::fmt::Write;
 
-    pub struct SpuUpload {
-        pub current_address: u16,
-    }
+use arrayvec::ArrayString;
 
-    impl SpuUpload {
-        const SPU_TRANSFER_ADDR: MemoryCell<u16> = MemoryCell::new(0x1F80_1DA6);
-        const DMA_BASE_ADDR: MemoryCell<u32> = MemoryCell::new(0x1F80_10F0);
-        const DMA_BLOCK_SIZE: MemoryCell<u32> = MemoryCell::new(0x1F80_10F4);
-        const DMA_CONTROL: MemoryCell<u32> = MemoryCell::new(0x1F80_10F8 );
+use crate::common::MemoryCell;
 
-        pub fn new() -> Self {
-            SpuUpload {
-                current_address: 0x1000
-            }
-        }
+pub struct SpuUpload {
+    pub current_address: u16,
+}
 
-        pub fn load(&mut self, data: &[u32]) -> SpuSample {
-            let sample = SpuSample::new(self.current_address);
-            for chunk in data.chunks(4096) {
-                let (chunk_ptr, chunk_length) = (chunk.as_ptr(), chunk.len());
-                Self::SPU_TRANSFER_ADDR.set(self.current_address);
-                Self::DMA_BASE_ADDR.set(chunk_ptr as u32);
-                Self::DMA_BLOCK_SIZE.set(chunk_length as u32);
-                Self::DMA_CONTROL.set(0x0100_0201);
-                while Self::DMA_CONTROL.get() > 0 { /* NOP */ }
-                self.current_address += chunk_length as u16;
-            }
+impl SpuUpload {
+    const SPU_TRANSFER_ADDR: MemoryCell<u16> = MemoryCell::new(0x1F80_1DA6);
+    const DMA_BASE_ADDR: MemoryCell<u32> = MemoryCell::new(0x1F80_10F0);
+    const DMA_BLOCK_SIZE: MemoryCell<u32> = MemoryCell::new(0x1F80_10F4);
+    const DMA_CONTROL: MemoryCell<u32> = MemoryCell::new(0x1F80_10F8 );
 
-            sample
+    pub fn new() -> Self {
+        SpuUpload {
+            current_address: 0x1000
         }
     }
 
-    pub struct SpuSample {
-        start_address: u16,
+    pub fn load(&mut self, data: &[u32], debug: &impl Fn(&str)) -> SpuSample {
+        let sample = SpuSample::new(self.current_address);
+        for (i, chunk) in data.chunks(4096).enumerate() {
+            let mut txt = ArrayString::<64>::new();
+            write!(txt, "Start: {i}");
+            debug(&txt);
+            let (chunk_ptr, chunk_length) = (chunk.as_ptr(), chunk.len());
+            Self::SPU_TRANSFER_ADDR.set(self.current_address);
+            Self::DMA_BASE_ADDR.set(chunk_ptr as u32);
+            Self::DMA_BLOCK_SIZE.set(chunk_length as u32);
+            Self::DMA_CONTROL.set(0x0100_0201);
+            let kek = Self::DMA_CONTROL.get();
+            while kek > 0 {
+                write!(txt, "; {kek}");
+                debug(&txt);
+            }
+            self.current_address += chunk_length as u16;
+        }
+
+        sample
+    }
+}
+
+pub struct SpuSample {
+    start_address: u16,
+}
+
+impl SpuSample {
+    const VOICE_START_ADDR: MemoryCell<u16> = MemoryCell::new(0x1F80_1C06);
+    const VOICE_REPEAT_ADDR: MemoryCell<u16> = MemoryCell::new(0x1F80_1C0E);
+    const VOICE_PITCH: MemoryCell<u16> = MemoryCell::new(0x1F80_1C04);
+    const VOICE_VOLUME_LEFT: MemoryCell<u16> = MemoryCell::new(0x1F80_1C00);
+    const VOICE_VOLUME_RIGHT: MemoryCell<u16> = MemoryCell::new(0x1F80_1C02);
+    const KEY_ON: MemoryCell<u32> = MemoryCell::new(0x1F80_1D88);
+
+    pub fn new(start_address: u16) -> Self {
+        SpuSample {
+            start_address
+        }
     }
 
-    impl SpuSample {
-        const VOICE_START_ADDR: MemoryCell<u16> = MemoryCell::new(0x1F80_1C06);
-        const VOICE_REPEAT_ADDR: MemoryCell<u16> = MemoryCell::new(0x1F80_1C0E);
-        const VOICE_PITCH: MemoryCell<u16> = MemoryCell::new(0x1F80_1C04);
-        const VOICE_VOLUME_LEFT: MemoryCell<u16> = MemoryCell::new(0x1F80_1C00);
-        const VOICE_VOLUME_RIGHT: MemoryCell<u16> = MemoryCell::new(0x1F80_1C02);
-        const KEY_ON: MemoryCell<u32> = MemoryCell::new(0x1F80_1D88);
-
-        pub fn new(start_address: u16) -> Self {
-            SpuSample {
-                start_address
-            }
-        }
-
-        pub fn play(&mut self) -> &Self {
-            Self::VOICE_START_ADDR.set(self.start_address);
-            Self::VOICE_REPEAT_ADDR.set(self.start_address);
-            Self::VOICE_PITCH.set(0x1000);
-            Self::VOICE_VOLUME_LEFT.set(0x3FFF);
-            Self::VOICE_VOLUME_RIGHT.set(0x3FFF);
-            Self::KEY_ON.set(0x00000001);
-            self
-        }
+    pub fn play(&mut self, debug: &impl Fn(&str)) -> &Self {
+        debug("puk");
+        Self::VOICE_START_ADDR.set(self.start_address);
+        Self::VOICE_REPEAT_ADDR.set(self.start_address);
+        Self::VOICE_PITCH.set(0x1000);
+        Self::VOICE_VOLUME_LEFT.set(0x3FFF);
+        Self::VOICE_VOLUME_RIGHT.set(0x3FFF);
+        Self::KEY_ON.set(0x00000001);
+        self
     }
 }
