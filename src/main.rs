11d595a6f827e327e8daf46eeadb1074939b7b42
Stanislav Seregin <stas_seregin@mail.ru>
2026-03-20 13:43:02 +0300

    Starts from simple counter

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,9 @@
 #![no_main]
 
 mod common;
-mod spu;
 
 use psx::gpu::VideoMode;
 use psx::{dprintln, Framebuffer};
-
-use crate::spu::{Spu, Sequencer, Song, Track, Pattern, Note};
-
-const SAMPLE_DATA_1: &[u8] = include_bytes_skip_take!("../samples/file_all.spu", 0, 13500);
-const SAMPLE_DATA_2: &[u8] = include_bytes_skip_take!("../samples/file_all.spu", 13600, 8400);
-const SAMPLE_DATA_3: &[u8] = include_bytes_skip_take!("../samples/file_all.spu", 22000);
 
 #[unsafe(no_mangle)]
 fn main() {
@@ -23,43 +16,12 @@ fn main() {
     let font = fb.load_default_font();
     let mut txt = font.new_text_box(txt_offset, res);
 
-    let mut spu = Spu::take().expect("Failed to init SPU");
-
-    let mut sequencer: Sequencer<4> = Sequencer::new(&mut spu.sampler);
-    sequencer.load_sample(0, SAMPLE_DATA_1).expect("Failed to load sample 1");
-    sequencer.load_sample(1, SAMPLE_DATA_2).expect("Failed to load sample 2");
-    sequencer.load_sample(2, SAMPLE_DATA_3).expect("Failed to load sample 3");
-
-    const SONG: Song<2, 2, 16> = Song::new(120)
-        .with_track(0, Track::new()
-            .with_pattern(0, Pattern::new()
-                .with_note(0, Note::new(1, 0x1000))
-                .with_note(4, Note::new(2, 0x800))
-                .with_note(8, Note::new(3, 0x1200))
-                .with_note(12, Note::new(1, 0x600))
-            )
-            .with_order(&[0, 0])
-        )
-        .with_track(1, Track::new()
-            .with_pattern(0, Pattern::new()
-                .with_note(2, Note::new(2, 0x400))
-                .with_note(6, Note::new(3, 0x900))
-                .with_note(10, Note::new(1, 0x1100))
-                .with_note(14, Note::new(2, 0x700))
-            )
-            .with_order(&[0, 0])
-        );
-
-    sequencer.play_song(&SONG);
-
+    let mut count: usize = 0;
     loop {
-        sequencer.update(&SONG);
-
         txt.reset();
-        let (pattern, row) = sequencer.get_position();
-        dprintln!(txt, "Tracker Playing");
-        dprintln!(txt, "Pattern: {}, Row: {}", pattern, row);
-        dprintln!(txt, "Status: {}", if sequencer.is_playing() { "Playing" } else { "Stopped" });
+
+        count += 1;
+        dprintln!(txt, "Count: {}", count);
 
         fb.draw_sync();
         fb.wait_vblank();
